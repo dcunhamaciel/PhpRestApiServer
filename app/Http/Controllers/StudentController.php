79de6f403796e6dc99557c2b9cb95ee064846d61
Diego Maciel <dcunhamaciel@gmail.com>
2022-05-31 08:09:21 -0300

    Refactoring

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -21,57 +21,45 @@ class StudentController extends Controller
         $student->course = $request->course;
         $student->save();
 
-        return response()->json([
-            "message" => "Student record created"
-        ], 201);
+        return response()->json(["message" => "Student record created"], 201);
     }
   
     public function getStudent($id) 
     {
         $student = Student::find($id);
 
-        if (isset($student)) {            
-            return response($student->toJson(JSON_PRETTY_PRINT), 200);
-        } else {
-            return response()->json([
-              "message" => "Student not found"
-            ], 404);
+        if (!isset($student)) {            
+            return response()->json(["message" => "Student not found"], 404);            
         }
+
+        return response($student->toJson(JSON_PRETTY_PRINT), 200);
     }
   
     public function updateStudent(Request $request, $id) 
     {
         $student = Student::find($id);
 
-        if (isset($student)) {
-            $student->name = is_null($request->name) ? $student->name : $request->name;
-            $student->course = is_null($request->course) ? $student->course : $request->course;
-            $student->save();
-    
-            return response()->json([
-                "message" => "Student updated successfully"
-            ], 200);
-        } else {
-            return response()->json([
-                "message" => "Student not found"
-            ], 404);
+        if (!isset($student)) {
+            return response()->json(["message" => "Student not found"], 404);
         }
+
+        $student->name = is_null($request->name) ? $student->name : $request->name;
+        $student->course = is_null($request->course) ? $student->course : $request->course;
+        $student->save();
+
+        return response()->json(["message" => "Student updated successfully"], 200);
     }
   
     public function deleteStudent($id) 
     {
         $student = Student::find($id);
 
-        if (isset($student)) {
-            $student->delete();
-
-            return response()->json([
-                "message" => "Student deleted"
-              ], 202);
-        } else {
-            return response()->json([
-                "message" => "Student not found"
-            ], 404);
+        if (!isset($student)) {
+            return response()->json(["message" => "Student not found"], 404);
         }
+
+        $student->delete();
+
+        return response()->json(["message" => "Student deleted"], 202);        
     }
 }
