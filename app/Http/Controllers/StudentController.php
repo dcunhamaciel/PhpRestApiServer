987b263bc138dac197c74deb43b4e8b105890e2f
gustcoder <gustavofaramos@gmail.com>
2022-06-01 14:15:31 -0300

    Ajustes de tipagem e sugestoes de boas praticas para API

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,62 +4,79 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class StudentController extends Controller
 {
-    public function getAllStudents() 
+    public function getAllStudents(): JsonResponse
     {
-        $students = Student::orderBy('id')->get()->toJson(JSON_PRETTY_PRINT);
+        $students = Student::orderBy('id')->get();
 
-        return response($students, 200);    
+        // quando retornar vazio podemos retornar um 204 (NO_CONTENT)
+        $httpStatusCode = empty($students) 
+            ? Response::HTTP_NO_CONTENT 
+            : Response::HTTP_OK;
+
+        return response()->json($students, $httpStatusCode);
     }
   
-    public function createStudent(Request $request) 
+    public function createStudent(Request $request): JsonResponse
     {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'course' => 'required|string',
+        ]);
+        
         $student = new Student;
         $student->name = $request->name;
         $student->course = $request->course;
         $student->save();
 
-        return response()->json(["message" => "Student record created"], 201);
+        return response()->json(["message" => "Student record created"], Response::HTTP_CREATED);
     }
   
-    public function getStudent($id) 
+    public function getStudent(int $id): JsonResponse
     {
         $student = Student::find($id);
 
-        if (!isset($student)) {            
-            return response()->json(["message" => "Student not found"], 404);            
+        if (empty($student)) {            
+            return response()->json(["message" => "Student not found"], Response::HTTP_NOT_FOUND);
         }
 
-        return response($student->toJson(JSON_PRETTY_PRINT), 200);
+        return response($student->toJson(JSON_PRETTY_PRINT), Response::HTTP_OK);
     }
   
-    public function updateStudent(Request $request, $id) 
+    public function updateStudent(Request $request, int $id): JsonResponse
     {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'course' => 'required|string',
+        ]);
+        
         $student = Student::find($id);
 
-        if (!isset($student)) {
-            return response()->json(["message" => "Student not found"], 404);
+        if (empty($student)) {
+            return response()->json(["message" => "Student not found"], Response::HTTP_NOT_FOUND);
         }
 
-        $student->name = is_null($request->name) ? $student->name : $request->name;
-        $student->course = is_null($request->course) ? $student->course : $request->course;
+        $student->name = $request->name;
+        $student->course = $request->course;
         $student->save();
 
-        return response()->json(["message" => "Student updated successfully"], 200);
+        return response()->json(["message" => "Student updated successfully"], Response::HTTP_OK);
     }
   
-    public function deleteStudent($id) 
+    public function deleteStudent(int $id): JsonResponse
     {
         $student = Student::find($id);
 
-        if (!isset($student)) {
-            return response()->json(["message" => "Student not found"], 404);
+        if (empty($student)) {
+            return response()->json(["message" => "Student not found"], Response::HTTP_NOT_FOUND);
         }
 
         $student->delete();
 
-        return response()->json(["message" => "Student deleted"], 202);        
+        return response()->json(["message" => "Student deleted"], Response::HTTP_ACCEPTED);        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('students', [\App\Http\Controllers\StudentController::class, 'getAllStudents']);
-Route::get('students/{id}', [\App\Http\Controllers\StudentController::class, 'getStudent']);
-Route::post('students', [\App\Http\Controllers\StudentController::class, 'createStudent']);
-Route::put('students/{id}', [\App\Http\Controllers\StudentController::class, 'updateStudent']);
-Route::delete('students/{id}', [\App\Http\Controllers\StudentController::class, 'deleteStudent']);
+Route::namespace('\App\Http\Controllers')->group(function()
+{
+    Route::get('students', 'StudentController@getAllStudents');
+    Route::get('students/{id}', 'StudentController@getStudent');
+    Route::post('students', 'StudentController@createStudent');
+    Route::put('students/{id}', 'StudentController@updateStudent');
+    Route::delete('students/{id}', 'StudentController@deleteStudent');
+});
