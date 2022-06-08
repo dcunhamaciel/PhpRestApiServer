40850da8a58ea0ddd63a03b213d2e22dc2d618a6
Diego Maciel <84419531+dcunhamaciel@users.noreply.github.com>
2022-06-08 08:25:59 -0300

    Tipagem retorno funções
    Co-authored-by: Gustavo Ramos <72817476+gustavoraamos@users.noreply.github.com>

--- app/Http/Requests/StudentRequest.php
+++ app/Http/Requests/StudentRequest.php
@@ -21,7 +21,7 @@ class StudentRequest extends FormRequest
      *
      * @return array<string, mixed>
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'name' => 'required|string|max:60',
